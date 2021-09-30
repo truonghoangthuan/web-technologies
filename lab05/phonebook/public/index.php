69a43db4409db5e95f02d411467a3fd04d9602aa
truonghoangthuan <tthun32@gmail.com>
2021-09-30 18:35:48 +0700

    30/09/2021 18h35

--- lab05/phonebook/public/index.php
+++ lab05/phonebook/public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-define('ROOTDIR', realpath(dirname(__DIR__)).DIRECTORY_SEPARATOR);
+define('ROOTDIR', realpath(dirname(__DIR__)) . DIRECTORY_SEPARATOR);
 define('APPNAME', 'My Phonebook');
 
 // Turn off error display in production
@@ -8,15 +8,15 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once ROOTDIR.'vendor\autoload.php';
-require_once ROOTDIR.'db.php';
+require_once ROOTDIR . 'vendor\autoload.php';
+require_once ROOTDIR . 'db.php';
 
 session_save_path('C:\xampp\tmp');
 session_start();
 
 use \App\Router;
 
-if (! ob_get_level()) {
+if (!ob_get_level()) {
     ob_start();
 }
 
@@ -28,8 +28,13 @@ Router::get('/login', '\App\Controllers\Auth\LoginController@showLoginForm');
 Router::post('/login', '\App\Controllers\Auth\LoginController@login');
 
 // Contact routes
-Router::get('/', '\App\Controllers\HomeController@index');
-Router::get('/home', '\App\Controllers\HomeController@index');
+Router::get('/', '\App\Controllers\ContactsController@index');
+Router::get('/home', '\App\Controllers\ContactsController@index');
+Router::get('/contacts/add', '\App\Controllers\ContactsController@add');
+Router::post('/contacts', '\App\Controllers\ContactsController@create');
+Router::get('/contacts/edit/(:num)', '\App\Controllers\ContactsController@edit');
+Router::post('/contacts/(:num)', '\App\Controllers\ContactsController@update');
+Router::post('/contacts/delete/(:num)', '\App\Controllers\ContactsController@delete');
 
 // 404 error
 Router::error('\App\Controllers\Controller@notFound');
@@ -37,4 +42,3 @@ Router::error('\App\Controllers\Controller@notFound');
 Router::dispatch();
 
 ob_end_flush();
-
